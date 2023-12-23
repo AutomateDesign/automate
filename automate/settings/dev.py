3a7f304e1cd6a000526dc9153f0361885c129176
AutomateDesign <me.kakar@gmail.com>
2023-12-22 21:45:23 -0500

    add database

--- automate/settings/dev.py
+++ automate/settings/dev.py
@@ -1,18 +1,23 @@
 from .base import *
+import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+
+
+DEBUG = False
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#*ntc(%wd-l^he8@*(&tk7lo8iw-919kov3u&2mwi#w0=jzn9-"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 
 
 try:
     from .local import *
 except ImportError:
     pass
+
--- automate/settings/production.py
+++ automate/settings/production.py
@@ -1,8 +1,19 @@
 from .base import *
 
+
+
 DEBUG = False
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+
 
 try:
     from .local import *
 except ImportError:
     pass
+
+
+
+
