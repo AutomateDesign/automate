73a2411f9b9041437c9316f62bd362c77c4b04d9
AutomateDesign <me.kakar@gmail.com>
2023-12-22 21:11:25 -0500

    initial commit

---
+++ automate/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-#*ntc(%wd-l^he8@*(&tk7lo8iw-919kov3u&2mwi#w0=jzn9-"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
